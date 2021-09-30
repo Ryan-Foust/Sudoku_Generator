bfef935213ca9bbbc3f5018486d22e2d4e97ab2a
rjf2621 <rjf2621@rit.edu>
2021-09-29 20:53:28 -0400

    Play.java added main method Setup.java added description of class

--- src/Play.java
+++ src/Play.java
@@ -1,2 +1,18 @@
 public class Play {
+    /**
+     * Need to work out a consistent coordinate system to be able to work with the user properly.
+     * ([Section], [Block], [Number to Put]) may work, but will need to trial a bunch of times to see how well this
+     * works. May get confusing because [Section] and [Block] would be labelled 0-8,
+     * while [Number to Put] would be 1-9
+     */
+
+    // This will be the main file to start on. Once started, will call Setup to set up the board.
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
 }
--- src/Setup.java
+++ src/Setup.java
@@ -1,2 +1,7 @@
 public class Setup {
+    /**
+     * This will set up a game of sudoku.
+     */
+
+
 }
